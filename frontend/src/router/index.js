204c73c1a6ea5c633cc163b3f000ba879279a39c
James456 <valorenid@gmail.com>
2022-12-03 19:02:59 +0545

    changes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AddCategory from '../views/Category/AddCategory'
 
 const routes = [
+  //Router for 
   {
     path: '/',
     name: 'home',
@@ -14,6 +16,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/admin/category/add',
+    name: 'AddCategory',
+    component: AddCategory
   }
 ]
 
