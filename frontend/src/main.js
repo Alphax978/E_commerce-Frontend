95fad2e87f1690f703caa6d6d67ad1e3e8251ea5
James456 <valorenid@gmail.com>
2023-02-05 22:07:38 +0545

    changes

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
-import PaperDashboard from "./plugins/paperDashboard";
-import "vue-notifyjs/themes/default.css";
+
 
 
 
 const app = createApp(App)
 app.use(router)
-app.use(PaperDashboard)
 app.mount('#app')
-
 
 
 
