c39ad659dc08cc1dee37b259a552f573af6fd221
James456 <valorenid@gmail.com>
2023-02-05 18:06:21 +0545

    changes

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+    <NavbarShow  />
+    <router-view>
+    </router-view>
+
+</div>
+</template>
+
+<script>
+import NavbarShow from './components/NavbarShow';
+// import FooterShow from './components/FooterShow';
+// import axios from 'axios';
+export default {
+  components: { NavbarShow},
+
+};
+</script>
+
+<style>
+html {
+  overflow-y: scroll;
+}
+</style>
