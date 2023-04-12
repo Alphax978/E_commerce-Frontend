c58beac07422c641eae74c89d3b065d0c6f6301e
James456 <valorenid@gmail.com>
2023-04-12 15:34:19 +0545

    changes

---
+++ frontend/src/components/CheckoutItems.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+            <h4>Cart <span class="price" style="color:black"><i class="fa fa-shopping-cart"></i> </span></h4>
+            <p>{{cartItem.product.name}} <span class="price">{{cartItem.product.price}}</span></p>
+            <hr>
+            <p>Total <span class="price" style="color:black"><b>${{totalCost.toFixec(2)}}</b></span></p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"CheckoutItem",
+    props:["cartItems"],
+}
+</script>
--- frontend/src/views/Payment/PaymentPage.vue
+++ frontend/src/views/Payment/PaymentPage.vue
@@ -1,7 +1,8 @@
 <template>
 <div>
 <NavbarShow/>
-    <div class="container">
+<br/>
+    <!-- <div class="container">
         <div class="row">
             <div class="col-12 text-center">
                 <h4 class="pt-3">Your Confirmed Products</h4>
@@ -39,16 +40,60 @@
             </button>
         </div>
 
-    </div> 
+    </div>  -->
 
+    <div class="row">
 
+    <div class="col-75">
+        <div class="container">
+            <form>
+                <div class="col-50">
+                    <h3>Payment</h3>
+                    <label>Accepted Cards</label>
+                    <div class="icon-container">
+                        <i class="fa fa-cc-visa" style="color:navy;"></i>
+                        <i class="fa fa-cc-amex" style="color:blue;"></i>
+                        <i class="fa fa-cc-mastercard" style="color:red;"></i>
+                        <i class="fa fa-cc-discover" style="color:orange;"></i>
+                    </div>
+                    <label for="price">Total</label>
+                    <input type="text" id="price" name="price" value="10">
+                    <label for="currency">Currency</label>
+                    <input type="text" id="currency" name="currency" placeholder="Enter Currency" value="USD">
+                    <label for="method">Payment Method</label>
+                    <input type="text" id="method" name="method" placeholder="Payment Method" value="PayPal">
+                    <label for="intent">Intent</label>
+                    <input type="text" id="intent" name="intent" value="Buy">
+                    <label for="description">Payment Description</label>
+                    <input type="text" id="description" name="description" placeholder="Payment Description">
 
+                </div>
+
+                <input type="submit" value="Continue to checkout" class="btn">
+            </form>
+        </div>
+    </div>
+
+    <div class="col-25" >
+       <div class="container">
+            <h4>Cart <span class="price" style="color:black"><i class="fa fa-shopping-cart"></i> </span></h4>
+            <div v-for="cartItem in cartItems" :key="cartItem.product.id">
+              <p>{{cartItem.product.name}} <span class="price">{{cartItem.product.price}}</span></p>
+              <hr>
+            </div>
+            <p>Total <span class="price" style="color:black"><b>${{totalCost.toFixed(2)}}</b></span></p>
+    </div>
+     
+    </div>
 </div>
+</div>
+
 </template>
 
 <script>
 import axios from 'axios';
 import NavbarShow from '../../components/NavbarShow'
+
 import paypal from '../../assets/paypal.png'
 // import KhaltiCheckout from "khalti-checkout-web";
 
@@ -134,25 +179,101 @@ export default {
 </script>
 
 <style scoped>
-a:link { text-decoration: none; }
-a:visited { text-decoration: none; }
-a:hover { text-decoration: none; }
-a:active { text-decoration: none; }
 
-#hess{
 
+.row {
+  display: -ms-flexbox; /* IE10 */
+  display: flex;
+  -ms-flex-wrap: wrap; /* IE10 */
+  flex-wrap: wrap;
+  margin: 0 -16px;
 }
 
-
-#cla{
-  background-color: #253B80;
-  color: whitesmoke;
+.col-25 {
+  -ms-flex: 25%; /* IE10 */
+  flex: 25%;
 }
 
-#cla:hover{
-  background-color: #169BD7;
+.col-50 {
+  -ms-flex: 50%; /* IE10 */
+  flex: 50%;
 }
 
+.col-75 {
+  -ms-flex: 75%; /* IE10 */
+  flex: 75%;
+}
+
+.col-25,
+.col-50,
+.col-75 {
+  padding: 0 16px;
+}
+
+.container {
+  background-color: #f2f2f2;
+  padding: 5px 20px 15px 20px;
+  border: 1px solid lightgrey;
+  border-radius: 3px;
+}
+
+input[type=text] {
+  width: 100%;
+  margin-bottom: 20px;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+}
+
+label {
+  margin-bottom: 10px;
+  display: block;
+}
+
+.icon-container {
+  margin-bottom: 20px;
+  padding: 7px 0;
+  font-size: 24px;
+}
+
+.btn {
+  background-color: #4CAF50;
+  color: white;
+  padding: 12px;
+  margin: 10px 0;
+  border: none;
+  width: 100%;
+  border-radius: 3px;
+  cursor: pointer;
+  font-size: 17px;
+}
+
+.btn:hover {
+  background-color: #45a049;
+}
+
+a {
+  color: #2196F3;
+}
+
+hr {
+  border: 1px solid lightgrey;
+}
+
+span.price {
+  float: right;
+  color: grey;
+}
+
+/* Responsive layout - when the screen is less than 800px wide, make the two columns stack on top of each other instead of next to each other (also change the direction - make the "cart" column go on top) */
+@media (max-width: 800px) {
+  .row {
+    flex-direction: column-reverse;
+  }
+  .col-25 {
+    margin-bottom: 20px;
+  }
+}
 
 
 
